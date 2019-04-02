47da2f2cebeb1674831d6875b5ab9f48bd48dec2
oscarochoa1 <46940987+oscarochoa1@users.noreply.github.com>
2019-04-02 18:58:25 -0500

    Update OscarOchoa_ejercicio10.py

--- OscarOchoa_ejercicio10.py
+++ OscarOchoa_ejercicio10.py
@@ -12,12 +12,12 @@ Inicio = '20080201'
 Final   = '20080201'
 datos9=pd.read_csv('https://hub.mybinder.org/user/computociencias-fisi2029-201910-cwhww83w/edit/Seccion_1/Fourier/Datos/transacciones2009.txt',sep="\t")
 datos["date [AST]"]=pd.to_datetime(datos["date [AST]"],format='%Y%m%d %H:%M:%S')
-datos.set_index(["date [AST]"],inplace=True)git
+datos.set_index(["date [AST]"],inplace=True)
 
 datos8=pd.read_csv('https://hub.mybinder.org/user/computociencias-fisi2029-201910-cwhww83w/edit/Seccion_1/Fourier/Datos/transacciones2008.txt',sep="\t")
 datos["date [AST]"]=pd.to_datetime(datos["date [AST]"],format='%Y%m%d %H:%M:%S')
-datos.set_index(["date [AST]"],inplace=True)git
+datos.set_index(["date [AST]"],inplace=True)
 
 datos10=pd.read_csv('https://hub.mybinder.org/user/computociencias-fisi2029-201910-cwhww83w/edit/Seccion_1/Fourier/Datos/transacciones2010.txt',sep="\t")
 datos["date [AST]"]=pd.to_datetime(datos["date [AST]"],format='%Y%m%d %H:%M:%S')
-datos.set_index(["date [AST]"],inplace=True)git
+datos.set_index(["date [AST]"],inplace=True)
